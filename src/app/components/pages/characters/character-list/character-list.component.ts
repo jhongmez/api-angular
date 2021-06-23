4c36ee854fecc0925f8d72b7e49e05e7ffa85d24
jhongmez <gmezjhon@gmail.com>
2021-06-23 11:00:18 -0500

    0.0.11 Interpolations - Get API

--- src/app/components/pages/characters/character-list/character-list.component.ts
+++ src/app/components/pages/characters/character-list/character-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
 import { Character } from '@app/shared/interfaces/character.interface';
 import { CharacterService } from '@app/shared/services/character.service';
 
@@ -24,31 +26,40 @@ export class CharacterListComponent implements OnInit {
 
 
   private pageNum = 1;
-  private query: string = "";
+  private query: string = '';
   private hideScrollHeight = 200;
   private showScrollHeight = 500;
 
-  constructor(private CharacterSvc: CharacterService) { 
-  }
+  constructor(
+    private CharacterSvc: CharacterService, 
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.getDataFromService();
   }
+  
 
-  private getDataFromService ():void {
+  private getDataFromService(): void {
     
     // take: El operador lo tomamos del modulo rxjs en la parte superior
     // ...: Spread Operator para agregar valores de una array en otro
     // info: Es si existe otra pagina para hacer la paginacion
     // results: Son los datos de los personajes
+    // ?. : Optional chaining para asegurar de que las propiedades existan antes de leerlas
     this.CharacterSvc
       .searchCharacters(this.query, this.pageNum)
       .pipe(take(1))
       .subscribe((res: any) => {
-        console.log('Response ->', res)
-        const {info, results} = res;
-        this.characters = [...this.characters, ...results];
-        this.info = info;
+
+        if(res ?.results?.length) {
+          const {info, results} = res;
+          this.characters = [...this.characters, ...results];
+          this.info = info;
+        } else {
+          this.characters = [];
+        }
+
       });
 
   }
