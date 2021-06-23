830e73f80362765276d242d082afd25ea7e98cb8
jhongmez <gmezjhon@gmail.com>
2021-06-23 09:31:00 -0500

    0.0.10 API data into character list

--- src/app/components/pages/characters/character-list/character-list.component.ts
+++ src/app/components/pages/characters/character-list/character-list.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '@app/shared/interfaces/character.interface';
+import { CharacterService } from '@app/shared/services/character.service';
+
+import { take } from 'rxjs/operators';
+
+type RequestInfo = {
+  next: string;
+};
 
 @Component({
   selector: 'app-character-list',
@@ -7,9 +15,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterListComponent implements OnInit {
 
-  constructor() { }
+  // Character[]: Esta es la interface que creamos
+  characters: Character[] = [];
+
+  info: RequestInfo = {
+    next: '',
+  };
+
+
+  private pageNum = 1;
+  private query: string = "";
+  private hideScrollHeight = 200;
+  private showScrollHeight = 500;
+
+  constructor(private CharacterSvc: CharacterService) { 
+  }
 
   ngOnInit(): void {
+    this.getDataFromService();
+  }
+
+  private getDataFromService ():void {
+    
+    // take: El operador lo tomamos del modulo rxjs en la parte superior
+    // ...: Spread Operator para agregar valores de una array en otro
+    // info: Es si existe otra pagina para hacer la paginacion
+    // results: Son los datos de los personajes
+    this.CharacterSvc
+      .searchCharacters(this.query, this.pageNum)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        console.log('Response ->', res)
+        const {info, results} = res;
+        this.characters = [...this.characters, ...results];
+        this.info = info;
+      });
+
   }
 
 }
