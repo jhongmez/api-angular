8da8bb41d3b3911afc78d7fdba36f10958cf327e
jhongmez <gmezjhon@gmail.com>
2021-06-22 23:48:20 -0500

    0.0.4 Modified routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   { 
     path: 'home', 
-    loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) 
+    loadChildren: () => 
+      import(
+        './components/pages/home/home.module'
+      ).then(m => m.HomeModule) 
   }, 
   { 
     path: 'character-list', 
-    loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) 
+    loadChildren: () => 
+      import(
+        './components/pages/characters/character-list/character-list.module'
+      ).then(m => m.CharacterListModule) 
   },
   { 
-    path: 'character-details', 
-    loadChildren: () => import('./components/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) 
+    path: 'character-details/:id', 
+    loadChildren: () => 
+      import(
+        './components/pages/characters/character-details/character-details.module'
+      ).then(m => m.CharacterDetailsModule) 
   },
 ];
 
